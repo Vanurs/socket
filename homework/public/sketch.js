e9eeee77d45b9e97082f1b5bc99ca8d4e78cbb42
Vanurs <76441985+Vanurs@users.noreply.github.com>
2024-02-08 18:52:49 +0000

    added gyro

--- homework/public/sketch.js
+++ homework/public/sketch.js
@@ -15,6 +15,23 @@ let recievedMouseY = 0;
 
 let button;
 
+//orientation
+//global variables
+let askButton;
+
+// device motion
+let accX = 0;
+let accY = 0; 
+let accZ = 0;
+let rrateX = 0;
+let rrateY = 0; 
+let rrateZ = 0;
+
+// device orientation
+let rotateDegrees = 0;
+let frontToBack = 0; 
+let leftToRight = 0; 
+
 function setup() {
   //Currently we make other people's drawing fit into our canvas, so when on portrait resolutions vs landscape things will look a little different/distorted
   //ratio fix... but then need to make a bunch of other UX decisions like whether you zoom into the canvas or center it somehow
@@ -66,13 +83,78 @@ function setup() {
   
   //Adding a mouse pressed event listener to the button 
   button.mousePressed(handleButtonPress); 
- 
+  rectMode(CENTER);
+  angleMode(DEGREES);
+  
+  //----------
+  //the bit between the two comment lines could be move to a three.js sketch except you'd need to create a button there
+  if(typeof DeviceMotionEvent.requestPermission === 'function' && typeof DeviceOrientationEvent.requestPermission === 'function'){
+    // iOS 13+
+    askButton = createButton('Permission');//p5 create button
+    askButton.mousePressed(handlePermissionButtonPressed);//p5 listen to mousePressed event
+  }else{
+    //if there is a device that doesn't require permission
+    window.addEventListener('devicemotion', deviceMotionHandler, true);
+    window.addEventListener('deviceorientation', deviceTurnedHandler, true)
+  }
+  
+  //----------
   background(255);
   noStroke();
 }
 
 function draw() {
+  let totalMovement = Math.abs(accX)+Math.abs(accY)+Math.abs(accZ);//movement in any direction
+  //set your own threshold for how sensitive you want this to be
+  if(totalMovement > 2){
+     background(255,0,0);
+  }else{
+     background(255);
+  }
+  
+  //Creating a tilt sensor mechanic that has a sort of boolean logic (on or off)
+  //if the phone is rotated front/back/left/right we will get an arrow point in that direction 
+  push();
+  translate(width/2,height/2);
+ 
+  if(frontToBack > 40){
+    push();
+    rotate(-180);
+    triangle(-30,-40,0,-100,30,-40);
+    pop();
+  }else if(frontToBack < 0){
+    push();
+    triangle(-30,-40,0,-100,30,-40);
+    pop();
+  }
+  
+  if(leftToRight > 20){
+    push();
+    rotate(90);
+    triangle(-30,-40,0,-100,30,-40);
+    pop();
+  }else if(leftToRight < -20){
+    push();
+    rotate(-90);
+    triangle(-30,-40,0,-100,30,-40);
+    pop();
+  }
+  pop();
+  
+  //Debug text
+  fill(0);
+  textSize(15);
+  
+  text("acceleration: ",10,10);
+  text(accX.toFixed(2) +", "+accY.toFixed(2)+", "+accZ.toFixed(2),10,40);
 
+  text("rotation rate: ",10,80);
+  text(rrateX.toFixed(2) +", "+rrateY.toFixed(2)+", "+rrateZ.toFixed(2),10,110);
+  
+  
+  text("device orientation: ",10,150);
+  text(rotateDegrees.toFixed(2) +", "+leftToRight.toFixed(2) +", "+frontToBack.toFixed(2),10,180);  
+  //---------------------------------------------
   if(drawIsOn){
     fill(r,g,b);
     circle(mouseX,mouseY,bSize);
@@ -153,6 +235,48 @@ function handleSliderInputChange(){
   bSize = sizeSlider.value();
 
   colorSwatch.style("background-color","rgb("+r+","+g+","+b+")");
+}
+
+function handlePermissionButtonPressed(){
+
+  DeviceMotionEvent.requestPermission()
+  .then(response => {
+    // alert(response);//quick way to debug response result on mobile, you get a mini pop-up
+    if (response === 'granted') {
+      window.addEventListener('devicemotion', deviceMotionHandler, true);
+    }
+  });
+
+  DeviceOrientationEvent.requestPermission()
+  .then(response => {
+    if (response === 'granted') {
+      // alert(response);//quick way to debug response result on mobile, you get a mini pop-up
+      window.addEventListener('deviceorientation', deviceTurnedHandler, true)
+    }
+  })
+  .catch(console.error);  
+}
+
+function deviceMotionHandler(event){
+  
+  accX = event.acceleration.x;
+  accY = event.acceleration.y;
+  accZ = event.acceleration.z;
+  
+  rrateZ = event.rotationRate.alpha;//alpha: rotation around z-axis
+  rrateX = event.rotationRate.beta;//rotating about its X axis; that is, front to back
+  rrateY = event.rotationRate.gamma;//rotating about its Y axis: left to right
+  
+}
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Window/deviceorientation_event
+function deviceTurnedHandler(event){
+  
+  //degrees 0 - 365
+  rotateDegrees = event.alpha; // alpha: rotation around z-axis
+  frontToBack = event.beta; // beta: front back motion
+  leftToRight = event.gamma; // gamma: left to right
+
 }
 
 //Events we are listening for
